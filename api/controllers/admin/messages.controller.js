4a4da86ade3aa867cec6f7fb8f88618cae309a0e
Yago Henrique <yagohsp@unipam.edu.br>
2019-10-07 10:45:27 -0300

    index/view/remove da funcao messages-admin adicionados

--- api/controllers/admin/messages.controller.js
+++ api/controllers/admin/messages.controller.js
@@ -1,17 +1,31 @@
 const Message = require('../../models/message.model');
 
 exports.index = async (req, res) => {
-  const { range, sort } = req.query;
-  console.log('req.query', req.query)
   try {
-    const messages = await Message.find({ limit: range }, {}, { sort });
-    messages
-    res.send(messages);
+
+    const messages = await Message.find();
+
+    return res.json(messages);
+    
   } catch (err) {
     res.status(400).send(err);
   }
 }
 
-exports.view = async (req, res) => { }
+exports.view = async (req, res) => {
 
-exports.remove = async (req, res) => { }
+  const { message_id } = req.params;
+
+  const messageFind = await Message.findById(message_id);
+
+  return res.json(messageFind);
+}
+
+exports.remove = async (req, res) => {
+
+  const { message_id } = req.params;
+
+  const messageFind = await Message.findByIdAndDelete(message_id);
+
+  return res.json(messageFind);
+}
