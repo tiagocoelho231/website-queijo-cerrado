8fdacf9113f1438524fd61a63bc079804416d382
Yago <yagohsp@unipam.edu.br>
2019-10-11 17:53:12 -0300

    menu dropdown

--- front/src/components/multi-menu/MenuBar.js
+++ front/src/components/multi-menu/MenuBar.js
@@ -4,32 +4,86 @@ import { Link } from 'react-router-dom';
 import './styles.scss'
 
 export default class MenuBar extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
       openMenu: false,
+      openSubMenu: false,
+      menuActive: false,
       menuOptions: [
-        { id: '1', name: 'Opção 1' },
-        { id: '2', name: 'Opção 2' },
-        { id: '3', name: 'Opção 3' },
+        {
+          id: '1',
+          name: 'Opção 1',
+          subMenu: [
+            'Sub Opção 1',
+            'Sub Opção 2',
+            'Sub Opção 3',
+          ]
+        },
+        {
+          id: '2',
+          name: 'Opção 2',
+          subMenu: [
+            'Sub Opção 1',
+            'Sub Opção 2',
+            'Sub Opção 3',
+          ]
+        },
+        {
+          id: '3',
+          name: 'Opção 3',
+          subMenu: [
+            'Sub Opção 1',
+            'Sub Opção 2',
+            'Sub Opção 3',
+          ]
+        }
       ]
     };
+
     this.toggleMenu = this.toggleMenu.bind(this);
+    this.toggleMenuClass = this.toggleMenuClass.bind(this);
+    this.openMenu = this.openMenu.bind(this);
   }
 
   toggleMenu() {
     this.setState(({ openMenu }) => ({ openMenu: !openMenu }));
-    console.log(this.state.menuOptions);
+  }
+
+  toggleMenuClass(tab) {
+    // this.state = { menuActive: tab };
+    this.setState({ menuActive: tab });
+  }
+
+  openMenu(id) {
+    console.log("teste");
   }
 
   render() {
-    const { openMenu, menuOptions } = this.state;
+    const { menuActive, menuOptions } = this.state;
 
-    const items = this.state.menuOptions.map((item) =>
-      <li key={item.id}><Link to="/">{item.name}</Link></li>
+    const items = menuOptions.map((item) =>
+      <div
+        onMouseEnter={this.toggleMenuClass.bind(this, item.id)}
+        onMouseLeave={this.toggleMenuClass.bind(this, null)}
+      >
+        <li key={item.id}>
+          <div className="menuOptions">
+
+            <Link to="/">{item.name}</Link>
+            <div className={`${menuActive === item.id ? 'triangle' : ''}`}></div>
+
+            <ul className={`${menuActive === item.id ? 'subOptionsActive' : 'subOptionsInactive'}`}>
+
+              {item.subMenu.map(element =>
+                <li>{element}</li>
+              )}
+            </ul>
+
+          </div>
+        </li>
+      </div>
     );
-
 
     return (
       <header>
