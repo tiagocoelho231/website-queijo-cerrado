7de75f7ba69d6014d470006c0390bb5909deeb28
PedroFernandes97 <55215532+PedroFernandes97@users.noreply.github.com>
2019-11-17 21:45:13 -0300

    Criação do layout da page - Pedro / Rogério

--- front/src/pages/Historias.js
+++ front/src/pages/Historias.js
@@ -2,8 +2,64 @@ import React from 'react'
 
 const Historias = () => {
   return (
-    <main>
-
+    <main class="historia-main">
+        <section className="historia-container-1">
+          <div className="item-1"> HISTÓRIAS QUE ENCANTAM </div>
+          <div className="item-2"> Neste espaço, você irá conhecer histórias encantadoras e inspiradoras, que envolvem o queijo não só como um produto, mas também como o resultado. Por meio de narrativas que buscam mostrar um pouco da personalidade e “raízes” dos produtores veremos o Queijo Minas Artesanal do Cerrado carregado de sinônimos e memórias surpreendentes. </div>
+        </section>
+        <section className="historia-container-2">
+            <div className="item-1">
+            E para mergulhar nessas histórias e descobrir pessoas inspiradoras, por trás de um produto tão tradicional, foi necessário, antes, conhecer a produção, um pouco sobre o queijo, sobre o produto fresco e o produto maturado e também compreender como era a lida diária dessas pessoas, em busca de entregar a seus consumidores um excelente queijo, agregado de sabor, tradição, cultura e também valor. Muito mais que contar histórias, as narrativas nos permitem buscar as raízes do queijo, possibilitando a criação de perspectivas, da divulgação do produto e também da importância de se tornar um associado e alcançar parcerias de órgãos importantes.
+            </div>
+        </section>
+        <section className="historia-container-3">
+          <div className="image-1">
+          <h5><img src={require('../img/artigo.jpg')} className="imagem-queijo-historias"/>Para mostrar o processo de produção, o pontapé inicial se deu na fazenda do presidente da APROCER, “O Casquinha”, da região de Cruzeiro da Fortaleza. Ele esbanja simpatia e mostrou, de forma sustentável, como cumprir exigências sem perder qualidade e tradição.</h5>
+          </div>
+          <div className="image-2">
+          <h5><img src={require('../img/artigo.jpg')} className="imagem-queijo-historias"/>A segunda entrevista/história aconteceu com o Sr. Geraldo Brandão, morador da região de Carmo do Paranaíba, que, com muita garra e resiliência, mostrou a importância de não desistir de um sonho, por mais que venham ventos contrários.</h5>
+          </div>
+          <div className="image-3">
+          <h5><img src={require('../img/artigo.jpg')} className="imagem-queijo-historias"/>A terceira história veio por meio de um empreendedor, cujo nome é Eudes Braga, também morador de Carmo do Paranaíba, que ensinou como agregar valor e multiplicar oportunidades, fazendo com que a própria produção se encarregue de crescer.</h5>
+          </div>
+          <div className="image-4">
+          <h5><img src={require('../img/artigo.jpg')} className="imagem-queijo-historias"/>. Já a quarta narrativa foi pura doçura, Sr. Antônio Lino, do município de Patrocínio, ensinou que, com muito jogo de cintura, é possível unir a produção de queijo com outras atividades, e ensinou, principalmente, que a receita da vida é nunca desistir, e selecionar ingredientes de primeira qualidade. </h5>
+          </div>
+          <div className="image-5">
+          <h5><img src={require('../img/artigo.jpg')} className="imagem-queijo-historias"/>A quinta narrativa nos apresenta ao Sr. Ronaldo PAM, do município de Patos de Minas, que, com simpatia e humildade, faz entender que não é preciso ser “grande” para sermos melhor, e que não é preciso ter muito para ser “Ouro” – literalmente.</h5>
+          </div>
+          <div className="image-6">
+          <h5><img src={require('../img/artigo.jpg')} className="imagem-queijo-historias"/>As histórias aqui mostradas evidenciam que é preciso valorizar as pessoas, seu valor humano, antes da “confecção de um produto”.</h5>
+          </div>
+        </section>
+        <section className="historia-container-4">
+          <div className="imagem-7">Imagem</div> 
+          <div className="titulo-text-7"><h3>Lorem ipsum dolor</h3></div>
+          <div className="text-7">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit
+            </p>
+          </div>
+        </section>
+        <section className="historia-container-5">
+          <div class="tirinha-1">Tirinha 1
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+          <div class="tirinha-2">Tirinha 2
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+          <div class="tirinha-3">Tirinha 3
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>          
+        </section>
+        <section className="historia-container-6">
+          <div class="historia-ilustrada">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </div>
+        </section>
     </main>
   )
 }
